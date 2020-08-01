3ea331caf719eff7e3fa8f73a45901d44f78a454
Patryk Marciszewski <patrykmarciszewski3@gmail.com>
2020-08-01 16:22:55 +0200

    update routes for delete todo

--- app/routes/todo.routes.js
+++ app/routes/todo.routes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post('/create_todo', todoController.create_todo);
 router.get('/todos/:user_id', todoController.load_todos);
 router.get('/todos/:user_id/:todo_id/', todoController.load_todo_data);
-router.delete('/todos/delete/:user_id/:todo_id', todoController.delete_todo);
+router.delete('/todos/:user_id/:todo_id', todoController.delete_todo);
 router.put('/todos/:user_id/:todo_id', todoController.edit_todo);
 
 module.exports = router;
