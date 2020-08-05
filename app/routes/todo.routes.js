da517dab86ebcc968b4a5570a19945f5d2a07b36
Patryk Marciszewski <patrykmarciszewski3@gmail.com>
2020-08-05 07:29:47 +0200

    added new method for update status todo

--- app/routes/todo.routes.js
+++ app/routes/todo.routes.js
@@ -8,5 +8,6 @@ router.get('/todos/:user_id', todoController.load_todos);
 router.get('/todos/:user_id/:todo_id/', todoController.load_todo_data);
 router.delete('/todos/:user_id/:todo_id', todoController.delete_todo);
 router.put('/todos/:user_id/:todo_id', todoController.edit_todo);
+router.put('/todos/status/:user_id/:todo_id', todoController.update_task_status);
 
 module.exports = router;
