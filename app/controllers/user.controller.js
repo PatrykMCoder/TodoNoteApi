bf739189efdf20fa8cb57ab634020a0f2f319d4b
Patryk Marciszewski <patrykmarciszewski3@gmail.com>
2020-09-02 00:30:20 +0200

    update else statemant

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -50,31 +50,7 @@ exports.edit_user = async(req, res, next) => {
                 }
             }
         }else{
-            if(!password){
-                UserModel.findOneAndUpdate({ _id: req.params.user_id }, { email: email, username: username }, {new: false}).then(user => {
-                    if(!user)
-                        return res.status(400).json(vm.ApiResponse(false, 400, "Can't update user data"));
-                    else{
-                        return res.status(201).json(vm.ApiResponse(true, 201, "User updated", user));
-                    }
-                }).catch(error => {
-                    return res.status(500).json(vm.ApiResponse(false, 500, "User not updated, catch error"));
-                });
-            }
-            else {
-                bcrypt.hash(password, salt, (err, hash) => {
-                    password = hash; 
-                });
-                
-                UserModel.findOneAndUpdate({ _id: req.params.user_id }, { email: email, username: username, password: password }, {new: false}).then(user => {
-                    if(!user)
-                        return res.status(400).json(vm.ApiResponse(false, 400, "Can't update user data"));
-                    else
-                        return res.status(201).json(vm.ApiResponse(true, 201, "User updated", user));
-                }).catch(error => {
-                    return res.status(500).json(vm.ApiResponse(false, 500, "User not updated, catch error"));;
-                });
-            }
+            return res.status(400).json(vm.ApiResponse(false, 400, "User data not found"));
         }
     }).catch(err => {
         return res.status(500).json(vm.ApiResponse(false, 500, "User not updated, catch error: ", err));
