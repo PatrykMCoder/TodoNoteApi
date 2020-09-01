28ef727b5a65f45339d7d77606102aa82afcaf8c
Patryk Marciszewski <patrykmarciszewski3@gmail.com>
2020-09-02 00:53:39 +0200

    fixed bug with update user data with password

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,5 +1,6 @@
 const UserModel = require('../models/user.model');
 const vm = require('v-response');
+const bcrypt =  require('bcrypt');
 
 exports.get_user_data = async(req, res, next) => {
     await UserModel.findOne({ _id: req.params.user_id}, {_id: 0, password: 0, __v: 0}).then(user_data => {
@@ -34,14 +35,16 @@ exports.edit_user = async(req, res, next) => {
                         return res.status(500).json(vm.ApiResponse(false, 500, "User not updated, catch error"));
                     });
                 } else {
-                    bcrypt.hash(password, salt, (err, hash) => {     
-                        UserModel.findOneAndUpdate({ _id: req.params.user_id }, { email: email, username: username, password: hash }, {new: false}).then(user => {
-                            if(!user)
-                                return res.status(400).json(vm.ApiResponse(false, 400, "Can't update user data"));
-                            else
-                                return res.status(201).json(vm.ApiResponse(true, 201, "User updated", user));
-                        }).catch(error => {
-                            return res.status(500).json(vm.ApiResponse(false, 500, "User not updated, catch error", error));;
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(password, salt, (err, hash) => { 
+                            UserModel.findOneAndUpdate({ _id: req.params.user_id }, { email: email, username: username, password: hash }, {new: false}).then(user => {
+                                if(!user)
+                                    return res.status(400).json(vm.ApiResponse(false, 400, "Can't update user data"));
+                                else
+                                    return res.status(201).json(vm.ApiResponse(true, 201, "User updated", user));
+                            }).catch(error => {
+                                return res.status(500).json(vm.ApiResponse(false, 500, "User not updated, catch error", error));;
+                            });
                         });
                     });
                 }
@@ -49,7 +52,7 @@ exports.edit_user = async(req, res, next) => {
         }else{
             return res.status(400).json(vm.ApiResponse(false, 400, "User data not found"));
         }
-    }).catch(err => {
+    }).catch(err => { 
         return res.status(500).json(vm.ApiResponse(false, 500, "User not updated, catch error: ", err));
     });
 };
