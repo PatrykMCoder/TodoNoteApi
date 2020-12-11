948c4a6ff2655211e3cae570dca6308de8f2a467
PatrykMCoder <patrykmarciszewski3@gmail.com>
2020-12-11 22:45:26 +0100

    fix no saved todo

--- app/controllers/todo.controller.js
+++ app/controllers/todo.controller.js
@@ -9,7 +9,7 @@ exports.create_todo = async(req, res, next) => {
             return res.status(409).json(vm.ApiResponse(false, 409, 'Title exist'));
         
         let title = req.body.title;
-        let todos = JSON.parse(req.body.todos.clean(""));
+        let todos = JSON.parse(req.body.todos);
         let user_id = req.body.user_id;
         let tag = req.body.tag;
 
@@ -52,7 +52,7 @@ exports.delete_todo = async(req, res, next) => {
 exports.edit_todo = async(req, res, next) => {
     let title = req.body.title;
     let tag = req.body.tag;
-    let todos = JSON.parse(req.body.todos.clean(""));
+    let todos = JSON.parse(req.body.todos);
 
     await TodoModel.findByIdAndUpdate({ user_id: ObjectId(req.params.user_id), _id: ObjectId(req.params.todo_id)}, {title: title, todos: todos, tag: tag}, {new: true}).then(update => {
         if (!update)
